59fe4130797fad6bc4615c8f0e992e5d315544f5
GahonaJordan <49221002+GahonaJordan@users.noreply.github.com>
2023-06-05 11:15:38 -0500

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -17,5 +17,44 @@ function izquierda() {
     }
 }
 
+//nodos
+const para = document.createElement("p");
+const node = document.createTextNode("Parrafo de prueba añadido mediante nodo");
+para.appendChild(node);
+const element = document.getElementById("div1");
+element.appendChild(para);
+
+function myFunction1() {
+    document.getElementById("p1.1").remove();
+}
+//colecciones
+
+function mifuncolecc1() {
+  const myCollection = document.getElementsByTagName("p");
+  for (let i = 0; i < myCollection.length; i++) {
+    myCollection[i].style.backgroundColor = "yellow";
+  }
+}
+
+function mifuncolecc2() {
+  const myCollection = document.getElementsByTagName("p");
+  for (let i = 0; i < myCollection.length; i++) {
+    myCollection[i].style.backgroundColor = "white";
+  }
+}
+
+function mifuncolecc3() {
+  const myCollection = document.getElementsByTagName("p");
+  for (let i = 0; i < myCollection.length; i++) {
+    myCollection[i].style.color = "blue";
+  }
+}
+
+function mifuncolecc4() {
+  const myCollection = document.getElementsByTagName("p");
+  for (let i = 0; i < myCollection.length; i++) {
+    myCollection[i].style.color = "black";
+  }
+}
 
 
