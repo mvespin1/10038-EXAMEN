fa72465451a425c6635066c48bb27ff7a0e0b6de
Ivonne Alexandra Jacome Sangoluisa <iajacome2@espe.edu.ec>
2023-06-04 23:59:31 -0500

    updateejemplo

--- js/funciones.js
+++ js/funciones.js
@@ -1,16 +1,21 @@
-function myMove() {
+//animaciones
+//** inclinado */
+function izquierda() {
     let id = null;
-    const elem = document.getElementById("animate");   
+    const elem = document.getElementById("animate");
     let pos = 0;
     clearInterval(id);
     id = setInterval(frame, 5);
     function frame() {
-      if (pos == 350) {
-        clearInterval(id);
-      } else {
-        pos++; 
-        elem.style.top = pos + "px"; 
-        elem.style.left = pos + "px"; 
-      }
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.right = pos + 'px';
+        }
     }
-  }
+}
+
+
+
