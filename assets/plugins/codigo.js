191726fb822c054191169d14eb68ea7e93d7b75d
mvespin1 <107557153+mvespin1@users.noreply.github.com>
2023-06-06 02:57:22 -0500

    EventListener y DOM Navigation

--- assets/plugins/codigo.js
+++ assets/plugins/codigo.js
@@ -33,4 +33,96 @@ function formsfuncion() {
       "forms1").innerHTML = txt;
 }
 
+//EJEMPLO 1 EVENT_LISTENER
+document.getElementById("boton").addEventListener("click", cambioColor);
+
+  function cambioColor() {
+    var div = document.getElementById("division");
+    div.style.backgroundColor = "skyblue";
+  }
+
+//EJEMPLO 2 EVENT_LISTENER
+var x = document.getElementById("boton2");
+x.addEventListener("mouseover", cambioColor1);
+x.addEventListener("click", cambioColor2);
+x.addEventListener("mouseout", cambioColor3);
+
+function cambioColor1() {
+  div = document.getElementById("division2");
+  div.style.backgroundColor = "skyblue";
+}
+
+function cambioColor2() {
+  div = document.getElementById("division2");
+  div.style.backgroundColor = "red";
+}
+
+function cambioColor3() {
+  div = document.getElementById("division2");
+  div.style.backgroundColor = "orange";
+}
+
+//EJEMPLO 3 EVENT_LISTENER
+window.addEventListener("click", cambiarTexto);
+
+function cambiarTexto() {
+  var text = document.getElementById("demo3").innerHTML = "Texto Cambiado";
+}
+
+//EJEMPLO 4 EVENT_LISTENER
+let texto1 = 'HOLA A ';
+let texto2 = 'TODOS';
+
+document.getElementById("boton4").addEventListener("click", function() {
+  unirTextos(texto1, texto2);
+});
+
+function unirTextos(a, b) {
+  document.getElementById("demo4").innerHTML = a + b;
+}
+
+//EJEMPLO 5 EVENT_LISTENER
+document.getElementById("divisionEj5_2").addEventListener("click", function() {
+  alert("CIRCULO NEGRO");
+}, false);
+
+document.getElementById("divisionEj5_1").addEventListener("click", function() {
+  alert("CIRCULO VERDE");
+}, false);
+
+document.getElementById("divisionEj5_4").addEventListener("click", function() {
+  alert("CIRCULO NEGRO");
+}, true);
+
+document.getElementById("divisionEj5_3").addEventListener("click", function() {
+  alert("CIRCULO VERDE");
+}, true);
+
+//EJEMPLO 6 EVENT_LISTENER
+document.getElementById("division6").addEventListener("mouseover", cambioColor6);
+
+  function cambioColor6() {
+    div = document.getElementById("division6");
+    div.style.backgroundColor = "skyblue";
+  }
+
+  function removerFuncionColor() {
+    document.getElementById("division6").removeEventListener("mouseover", cambioColor6);
+    div.style.backgroundColor = "greenyellow";
+
+  }
+
+//EJEMPLO 1 NAVEGATION NODE
+var valorNodoHijo = document.getElementById("hijo1").firstChild.nodeValue;
+document.getElementById("hijo2").innerHTML = valorNodoHijo;
+
+//EJEMPLO 4 NAVEGATION NODE
+document.getElementById("titulo").innerHTML = document.getElementById("parrafo").nodeName;
+
+//EJEMPLO 5 NAVEGATION NODE
+document.getElementById("parrafoEj4_1").innerHTML = document.getElementById("tituloEj4_1").nodeType;
+document.getElementById("parrafoEj4_2").innerHTML = document.getElementById("tituloEj4_2").nodeType;
+document.getElementById("parrafoEj4_3").innerHTML = document.getElementById("tituloEj4_3").nodeType;
+
+
 intro.style.backgroundColor = '#00FFFF';
